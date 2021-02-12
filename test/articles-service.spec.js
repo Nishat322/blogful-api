8d9c009a824c8836a63154c8233fab334a35db7d
Nishat Hoque <hoquen0397@gmail.com>
2021-02-12 14:53:43 -0500

    getallarticles endpoint test

--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -10,16 +10,22 @@ describe('Articles service object', function(){
     let db;
     let testArticles = [
         {
+            id: 1,
             title: 'First test post!',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?'
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
+            date_published: new Date('2029-01-22T16:28:32.615Z')
         },
         {
+            id: 2,
             title: 'Second test post!',
-            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.'
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.',
+            date_published: new Date('2100-05-22T16:28:32.615Z')
         },
         {
+            id: 3,
             title: 'Third test post!',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, voluptate? Necessitatibus, reiciendis? Cupiditate totam laborum esse animi ratione ipsa dignissimos laboriosam eos similique cumque. Est nostrum esse porro id quaerat.'
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, voluptate? Necessitatibus, reiciendis? Cupiditate totam laborum esse animi ratione ipsa dignissimos laboriosam eos similique cumque. Est nostrum esse porro id quaerat.',
+            date_published: new Date('1919-12-22T16:28:32.615Z')
         }
     ];
 
@@ -29,6 +35,8 @@ describe('Articles service object', function(){
             'connection': process.env.TEST1_DB_URL,
         });
     });
+
+    before(() => db('blogful_articles').truncate());
 
     before(() => {
         return db   
@@ -40,6 +48,15 @@ describe('Articles service object', function(){
 
     describe('getAllArticles()', () => {
         it('resolves all articles from \'blogful_articles\' table', () => {
+            return ArticlesService.getAllArticles(db)
+                .then(actual => {
+                    expect(actual).to.eql(testArticles.map(article => ({
+                        id: article.id,
+                        title: article.title,
+                        content: article.content,
+                        date_published: new Date(article.date_published)
+                    })));
+                });
         });
     });
 });
