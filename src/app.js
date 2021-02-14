dae9f9f50238bcb52d458f4e9823d69422e14001
Nishat Hoque <hoquen0397@gmail.com>
2021-02-14 16:51:46 -0500

    refactored

--- src/app.js
+++ src/app.js
@@ -6,10 +6,10 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-const ArticlesService = require('./articles/articles-service');
+const articlesRouter = require('./articles/articles-router');
+const errorHandler = require('./errorHandler');
 
 const app = express();
-const jsonParser = express.json();
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
@@ -19,57 +19,11 @@ app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
 
-app.get('/articles', (req,res,next) => {
-    const knexInstance = req.app.get('db');
-    ArticlesService.getAllArticles(knexInstance)
-        .then(articles => {
-            res.json(articles);
-        })
-        .catch(next);
-});
+app.get('/', (req,res) => {
+    res.status(200).send('Hello, world!');
+  });
 
-app.get('/articles/:article_id', (req,res,next) => {
-    const knexInstance = req.app.get('db');
-    const {article_id} = req.params;
-    ArticlesService.getById(knexInstance, article_id)
-        .then(article => {
-            if(!article){
-                return res
-                    .status(404)
-                    .json({error: {message: 'Article doesn\'t exist'}});
-            }
-            res.json(article);
-        })
-        .catch(next);
-});
-
-app.post('/articles', jsonParser, (req,res,next) => {
-    const {title, content, style} = req.body;
-    const newArticle = {title, content, style};
-    const knexInstance = req.app.get('db');
-    ArticlesService.insertArticle(knexInstance,newArticle)
-        .then(article => {
-            res
-                .status(201)
-                .location(`/articles/${article.id}`)
-                .json(article);
-        })
-        .catch(next);
-});
-
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
-});
-
-app.use(function errorHandler(error, req, res, next) {
-    let response;
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } };
-    } else {
-        console.error(error);
-        response = { message: error.message, error };
-    }
-    res.status(500).json(response);
-});
+app.use('/', articlesRouter);
+app.use(errorHandler);
     
 module.exports = app;
